e0c79e90686d8946d01f365b4ae3cad22474e012
Paul Fauth-Mayer <pf020@hdm-stuttgart.de>
2020-06-01 14:33:28 +0200

    handle singleton printing

--- src/printer.cpp
+++ src/printer.cpp
@@ -2,6 +2,7 @@
 
 void scm_print(scmObject object)
 {
+  std::cout << "print tag: " << object->tag << '\n';
   switch (object->tag) {
     case TAG_INTEGER:
       std::cout << object->u.intValue;
@@ -11,6 +12,15 @@ void scm_print(scmObject object)
       break;
     case TAG_SYMBOL:
       std::cout << object->u.stringValue;
+      break;
+    case TAG_NIL:
+      std::cout << "()";
+      break;
+    case TAG_TRUE:
+      std::cout << "#t";
+      break;
+    case TAG_FALSE:
+      std::cout << "#f";
       break;
     default:
       std::cout << "not yet implemented: " << object->tag;
