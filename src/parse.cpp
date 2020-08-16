43de03172784e051ecf965fc9c0155086c322a07
Paul Fauth-Mayer <paulfauthmayer@gmail.com>
2020-08-16 12:51:45 +0200

    nicer regex

--- src/parse.cpp
+++ src/parse.cpp
@@ -7,34 +7,41 @@
 #include "memory.hpp"
 #include "scheme.hpp"
 
+// CHECK FUNCTIONS
+
 bool isFloat(std::string str)
 {
-  std::regex pattern("^[0-9]+.[0-9]*$");
+  std::regex pattern(R"(^[0-9]+\.[0-9]*$)");
   return std::regex_match(str, pattern);
 }
 
 bool isInt(std::string str)
 {
-  std::regex pattern("^[0-9]+$");
+  std::regex pattern(R"(^[0-9]+$)");
   return std::regex_match(str, pattern);
 }
 
 bool isString(std::string str)
 {
-  std::regex pattern("^\".*\"$");
+  std::regex pattern(R"(^\".*\"$)");
   return std::regex_match(str, pattern);
 }
 
 bool isSymbol(std::string str)
 {
-  std::regex pattern("^[^\\s^\\d]\\S*$");
+  std::regex pattern(R"(^[^\s^\d]\S*$)");
   return std::regex_match(str, pattern);
 }
 
+// LEXING
+
 std::vector<std::string> splitLine(std::string line)
 {
+  /**
+   * The lexer is implemented on top of regular expressions!
+   */
   std::vector<std::string> v;
-  std::regex re("(\".*\"|[\\+\\/\\*\\-\\%\\(\\)]|[\\w\\d]+)");
+  std::regex re(R"(\".*\"|[\+\/\*\-\%\(\)]|[\w\d]+)");
   for (std::sregex_iterator i = std::sregex_iterator(line.begin(), line.end(), re); i != std::sregex_iterator(); i++) {
     v.push_back(std::smatch(*i).str());
   }
