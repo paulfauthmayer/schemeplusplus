baffb407fc98c552d60f2ecdd10948a015e8111b
Paul Fauth-Mayer <paulfauthmayer@gmail.com>
2020-08-22 16:54:05 +0200

    fix environment getter & setter

--- src/environment.cpp
+++ src/environment.cpp
@@ -1,5 +1,7 @@
 #include "environment.hpp"
+#include <exception>
 #include <iostream>
+#include <loguru.hpp>
 #include <vector>
 #include "memory.hpp"
 #include "scheme.hpp"
@@ -8,20 +10,26 @@ namespace scm {
 
 Object* getVariable(Environment& env, Object* key)
 {
-  auto found = std::find_if(env.bindings.begin(), env.bindings.end(), [key](Object* binding) {
-    return (getCar(binding) == getCar(key));
-  });
-  return (found != env.bindings.end()) ? *found : NULL;
+  auto lambda = [key](Object* binding) {
+    return (getStringValue(getCar(binding)) == getStringValue(key));
+  };
+  auto found = std::find_if(env.bindings.begin(), env.bindings.end(), lambda);
+
+  return (found != env.bindings.end()) ? getCdr(*found) : NULL;
 }
 
 void define(Environment& env, Object* key, Object* value)
 {
   auto currentDefinition = getVariable(env, key);
-  if (currentDefinition == NULL)
-    env.bindings.push_back(newCons(key, value));
-  else
+  if (currentDefinition == NULL) {
+    Object* definition{newCons(key, value)};
+    env.bindings.push_back(definition);
+    DLOG_F(INFO, "define %s := %s", toString(key).c_str(), toString(value).c_str());
+  }
+  else {
     std::get<ConsValue>(currentDefinition->value).cdr = value;
-  return;
+    std::cout << "redefine " << toString(key) << '\n';
+  }
 }
 
 }  // namespace scm
